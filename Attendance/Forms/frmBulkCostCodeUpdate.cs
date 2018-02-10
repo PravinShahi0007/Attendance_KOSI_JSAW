dc42831cd3a1364aa30ef1cf39ccd1558d6966f0
anand1606 <anand.j.acharya@gmail.com>
2018-02-10 18:08:57 +0530

    10-02-2018 : BugFixed : Bulk Cost Code ->Valid From

--- Attendance/Forms/frmBulkCostCodeUpdate.cs
+++ Attendance/Forms/frmBulkCostCodeUpdate.cs
@@ -186,7 +186,7 @@ namespace Attendance.Forms
                             {
                                 
                                 cmd.Connection = con;
-                                string sql = "Insert into MastCostCodeEmp (EmpUnqID,CostCode,ValidFrom,AddDt,AddID) Values ('{0}','{1}',GetDate(),'{2}','{3}')";
+                                string sql = "Insert into MastCostCodeEmp (EmpUnqID,CostCode,ValidFrom,AddDt,AddID) Values ('{0}','{1}','{2}',GetDate(),'{3}')";
                                 sql = string.Format(sql, tEmpUnqID, tCostCode.ToUpper(),
                                     tdt.ToString("yyyy-MM-dd"),
                                     Utils.User.GUserID);
